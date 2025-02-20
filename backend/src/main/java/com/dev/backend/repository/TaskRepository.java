5c4e0132818b53d80c88d520acc0bf4cea8ab9b5
Carlos Oliveira <caduolivera17@gmail.com>
2025-02-20 15:14:43 -0300

    creating repository

---
+++ backend/src/main/java/com/dev/backend/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.dev.backend.repository;
+
+import com.dev.backend.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
---
+++ backend/src/main/java/com/dev/backend/service/TaskService.java
@@ -0,0 +1,10 @@
+package com.dev.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+}
